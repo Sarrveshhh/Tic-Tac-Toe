bb7f4bb1ee4f37691dde27c9b5a6756a6330125d
Sarrveshhh <sarrvesharyaa@gmail.com>
2023-06-01 18:40:25 +0530

    added basic css and grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="heading">TIC TAC TOE GAME!</div>
+
+      <div className="grid">
+        <div className="box" id="box1"></div>
+        <div className="box" id="box2"></div>
+        <div className="box" id="box3"></div>
+        <div className="box" id="box4"></div>
+        <div className="box" id="box5"></div>
+        <div className="box" id="box6"></div>
+        <div className="box" id="box7"></div>
+        <div className="box" id="box8"></div>
+        <div className="box" id="box9"></div>
+      </div>
+    </>
   );
 }
 
